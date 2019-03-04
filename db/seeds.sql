2aaea16a06308b788aab45811db0c57b9dc3f962
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-03 20:13:55 -0700

    Added code for config, controller, db, models, views. Save before testing connect

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+SELECT * FROM burgers_db.burgers;
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Final Kraut Down Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Humpty Dumpty Burger", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Killer Burger", 0);
